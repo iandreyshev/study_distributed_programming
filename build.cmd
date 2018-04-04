8878864deffcf5e8b1ff72a8b5f70362395003f1
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2018-04-04 12:47:58 +0300

    Улучшен Build script

--- build.cmd
+++ build.cmd
@@ -1,41 +1,66 @@
 @ECHO OFF
 
-set VERSION=%~1
-set BUILD_FOLDER_NAME=v%VERSION%
+SET INVALID_ARGS_ERROR_CODE=1
+SET BUILD_ERROR=2
 
-set BACKEND_NAME=backend
-set BACKEND_SRC=src/%BACKEND%
-set BACKEND_OUT=%BUILD_FOLDER_NAME%/%BACKEND_NAME%
-set BACKEND_OUT_FROM_PROJ=../../%BACKEND_OUT%
+SET VERSION=%~1
+SET BUILD_DIR=v%VERSION%
+SET CONFIG_DIR=%BUILD_DIR%/config
 
-set FRONTEND_NAME=frontend
-set FRONTEND_SRC=src/%FRONTEND_NAME%
-set FRONTEND_OUT=%BUILD_FOLDER_NAME%/%FRONTEND_NAME%
-set FRONTEND_OUT_FROM_PROJ=../../%FRONTEND_OUT%
+SET BACKEND_NAME=backend
+SET BACKEND_SRC=src/%BACKEND_NAME%
+SET BACKEND_OUT=%BUILD_DIR%/%BACKEND_NAME%
+SET BACKEND_OUT_FROM_PROJ=../../%BACKEND_OUT%
+SET BACKEND_WINDOW_NAME=%BACKEND_NAME% v%VERSION%
 
-CALL :MakeOutDir
-CALL :Build                                                       
+SET FRONTEND_NAME=frontend
+SET FRONTEND_SRC=src/%FRONTEND_NAME%
+SET FRONTEND_OUT=%BUILD_DIR%/%FRONTEND_NAME%
+SET FRONTEND_OUT_FROM_PROJ=../../%FRONTEND_OUT%
+SET FRONTEND_WINDOW_NAME=%FRONTEND_NAME% v%VERSION%
+
+IF "%VERSION%"=="" (
+  GOTO InvalidArgs
+)
+
+CALL :Build
+CALL :CreateConfig
 CALL :CreateRunScript
 CALL :CreateStopScript
 
-:MakeOutDir
-mkdir %BUILD_FOLDER_NAME%
+ECHO Build completed!
 EXIT /B 0
 
 :Build
-dotnet build %BACKEND_SRC% -o %BACKEND_OUT_FROM_PROJ%
-dotnet build %FRONTEND_SRC% -o %FRONTEND_OUT_FROM_PROJ%
-EXIT /B 0
+  dotnet build %BACKEND_SRC% -o %BACKEND_OUT_FROM_PROJ%
+  dotnet build %FRONTEND_SRC% -o %FRONTEND_OUT_FROM_PROJ%
+  EXIT /B 0
+  
+:CreateConfig
+  md "%CONFIG_DIR%"
+  @ECHO > %CONFIG_DIR%/%FRONTEND_NAME%Config.cfg
+  @ECHO > %CONFIG_DIR%/%BACKEND_NAME%Config.cfg
+  EXIT /B 0
 
 :CreateRunScript
-(
-@echo start dotnet %FRONTEND_NAME%/%FRONTEND_NAME%.dll
-@echo start dotnet %BACKEND_NAME%/%BACKEND_NAME%.dll
-) > %BUILD_FOLDER_NAME%/run.cmd
-EXIT /B 0
+  (
+    @ECHO start "%FRONTEND_WINDOW_NAME%" dotnet %FRONTEND_NAME%/%FRONTEND_NAME%.dll
+    @ECHO start "%BACKEND_WINDOW_NAME%" dotnet %BACKEND_NAME%/%BACKEND_NAME%.dll
+  ) > %BUILD_DIR%/run.cmd
+  EXIT /B 0
 
 :CreateStopScript
-@echo dotnet > %BUILD_FOLDER_NAME%/stop.cmd
-EXIT /B 0
+  (
+    @ECHO @ECHO OFF
+    @ECHO
+    @ECHO taskkill /IM dotnet.exe
+  ) > %BUILD_DIR%/stop.cmd
+  EXIT /B 0
 
-                              
+:InvalidArgs
+  ECHO Build version not found...
+  EXIT /B %INVALID_ARGS_ERROR_CODE%
+
+:BuildError
+  ECHO Error during build project...
+  EXIT /B %BUILD_ERROR%
